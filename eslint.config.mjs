d25fb149b8019985b4fc29052ff2aef0fb352f5f
Melle Boersma <git@m10a.com>
2024-08-17 22:35:18 +0200

    Add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,13 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     ignores: ['dist/*']
-  }
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 );
